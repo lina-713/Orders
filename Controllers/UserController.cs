4030936ae5a85fab195914f348ea5bd0067d6269
lina-713 <alinav2048@gmail.com>
2024-07-19 16:40:20 +0300

    Добавьте файлы проекта.

---
+++ Controllers/UserController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Test.Controllers
+{
+    [Authorize]
+    [ApiController]
+    [Route("[controller]")]
+    public class UserController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(ILogger<UserController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<User> Get()
+        {
+            return Enumerable.Range(1, 5).Select(index => new User
+            {
+                Id = 2,
+                Name = "Alina",
+                Login = "alinavin"
+            })
+            .ToArray();
+        }
+    }
+}
